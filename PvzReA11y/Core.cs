38582d7ec6ba514d6d4e303a5731854f934afbf2
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-26 01:16:37 +0800

    init MelonLoader proj
    Using: https://github.com/TrevTV/MelonLoader.VSWizard/
    V1.3.1

---
+++ PvzReA11y/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(PvzReA11y.Core), "PvzReA11y", "1.0.0", "inkydragon@Github", null)]
+[assembly: MelonGame("PopCap Games", "PvZ Replanted")]
+
+namespace PvzReA11y
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
