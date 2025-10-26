615522a1ae2addd34e155317643b6c1617749fa6
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-26 23:33:53 +0800

    hook: 尝试挂钩按钮选择

---
+++ PvzReA11y/A11yPatch/SelectableBinderPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Il2CppReloaded.Binders;
+using UnityEngine.EventSystems;
+
+namespace PvzReA11y.A11yPatch;
+
+[HarmonyPatch(typeof(SelectableBinder))]
+internal class SelectableBinderPatch
+{
+    [HarmonyPatch("OnSelect")]
+    [HarmonyPostfix]
+    static void OnSelect(BaseEventData eventData)
+    {
+        // Core.gLogger.Msg($"Board.OnSelect(eventData={eventData})");
+    }
+}
