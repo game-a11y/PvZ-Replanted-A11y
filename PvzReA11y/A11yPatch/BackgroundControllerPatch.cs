43f67cf759b0b98d6462662d30be54060b956aca
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-28 15:04:26 +0800

    hook: 关闭部分查询函数

--- PvzReA11y/A11yPatch/BackgroundControllerPatch.cs
+++ PvzReA11y/A11yPatch/BackgroundControllerPatch.cs
@@ -22,31 +22,31 @@ public class BackgroundControllerPatch
         Core.gLogger.Msg("BackgroundController.Awake called");
     }
 
-    [HarmonyPostfix]
-    [HarmonyPatch("Enable1SodRow")]
-    public static void Enable1SodRow_Postfix(bool value)
-    {
-        Core.gLogger.Msg($"BackgroundController.Enable1SodRow({value})");
-    }
+    // [HarmonyPostfix]
+    // [HarmonyPatch("Enable1SodRow")]
+    // public static void Enable1SodRow_Postfix(bool value)
+    // {
+    //     Core.gLogger.Msg($"BackgroundController.Enable1SodRow({value})");
+    // }
 
-    [HarmonyPostfix]
-    [HarmonyPatch("Enable3SodRow")]
-    public static void Enable3SodRow_Postfix(bool value)
-    {
-        Core.gLogger.Msg($"BackgroundController.Enable3SodRow({value})");
-    }
+    // [HarmonyPostfix]
+    // [HarmonyPatch("Enable3SodRow")]
+    // public static void Enable3SodRow_Postfix(bool value)
+    // {
+    //     Core.gLogger.Msg($"BackgroundController.Enable3SodRow({value})");
+    // }
 
-    [HarmonyPostfix]
-    [HarmonyPatch("EnableNightGrave")]
-    public static void EnableNightGrave_Postfix(bool enable)
-    {
-        Core.gLogger.Msg($"BackgroundController.EnableNightGrave({enable})");
-    }
+    // [HarmonyPostfix]
+    // [HarmonyPatch("EnableNightGrave")]
+    // public static void EnableNightGrave_Postfix(bool enable)
+    // {
+    //     Core.gLogger.Msg($"BackgroundController.EnableNightGrave({enable})");
+    // }
 
-    [HarmonyPostfix]
-    [HarmonyPatch("EnableBowlingLine")]
-    public static void EnableBowlingLine_Postfix(bool enable, float x)
-    {
-        Core.gLogger.Msg($"BackgroundController.EnableBowlingLine({enable}, x={x})");
-    }
+    // [HarmonyPostfix]
+    // [HarmonyPatch("EnableBowlingLine")]
+    // public static void EnableBowlingLine_Postfix(bool enable, float x)
+    // {
+    //     Core.gLogger.Msg($"BackgroundController.EnableBowlingLine({enable}, x={x})");
+    // }
 }
