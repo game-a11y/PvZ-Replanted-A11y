1261e9e8718af9eb0f691fd7701b08e1acedaac9
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-28 14:31:06 +0800

    hook: GameplayOptionsMenu.OnEnable

---
+++ PvzReA11y/A11yPatch/GameplayOptionsMenuPatch.cs
@@ -0,0 +1,17 @@
+using HarmonyLib;
+using Il2CppReloaded.UI;
+using MelonLoader;
+using PvzReA11y.A11y;
+
+namespace PvzReA11y.A11yPatch;
+
+[HarmonyPatch(typeof(GameplayOptionsMenu))]
+public class GameplayOptionsMenuPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(GameplayOptionsMenu.OnEnable))]
+    public static void OnEnable_Postfix()
+    {
+        MelonLogger.Msg($"GameplayOptionsMenu.OnEnable called");
+    }
+}
