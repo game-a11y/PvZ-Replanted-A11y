8c34e58a6bb34124f22bcb51ee11ef4d5c39b8c0
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-28 14:39:18 +0800

    hook: HelpCarousel

---
+++ PvzReA11y/A11yPatch/HelpCarouselPatch.cs
@@ -0,0 +1,44 @@
+using HarmonyLib;
+using Il2CppUI.Scripts;
+using UnityEngine;
+
+namespace PvzReA11y.A11yPatch;
+
+[HarmonyPatch(typeof(HelpCarousel))]
+public class HelpCarouselPatch
+{
+    [HarmonyPatch("OnEnable")]
+    [HarmonyPostfix]
+    public static void OnEnable_Postfix()
+    {
+        Core.gLogger.Msg($"HelpCarousel.OnEnable()");
+    }
+
+    [HarmonyPatch("NextScreen")]
+    [HarmonyPostfix]
+    public static void NextScreen_Postfix()
+    {
+        Core.gLogger.Msg($"HelpCarousel.NextScreen()");
+    }
+
+    [HarmonyPatch("PreviousScreen")]
+    [HarmonyPostfix]
+    public static void PreviousScreen_Postfix()
+    {
+        Core.gLogger.Msg($"HelpCarousel.PreviousScreen()");
+    }
+
+    [HarmonyPatch("SetPageLabel")]
+    [HarmonyPostfix]
+    public static void SetPageLabel_Postfix()
+    {
+        Core.gLogger.Msg($"HelpCarousel.SetPageLabel()");
+    }
+
+    [HarmonyPatch("SetPage")]
+    [HarmonyPostfix]
+    public static void SetPage_Postfix(int page)
+    {
+        Core.gLogger.Msg($"HelpCarousel.SetPage(page={page})");
+    }
+}
