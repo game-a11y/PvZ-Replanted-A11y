6fac20577b20b07d5f201cc5c077a0fe40a0ba20
Chengyu HAN <cyhan.dev@outlook.com>
2025-10-29 16:01:57 +0800

    hook: PanelView.Show

---
+++ PvzReA11y/A11yPatch/Tekly/PanelViewPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Il2CppTekly.PanelViews;
+
+namespace PvzReA11y.A11yPatch.Tekly;
+
+[HarmonyPatch(typeof(PanelView))]
+internal class PanelViewPatch
+{
+    [HarmonyPatch(nameof(PanelView.Show))]
+    [HarmonyPostfix]
+    public static void Show_Postfix(string context, PanelData data = null)
+    {
+        Core.gLogger.Msg($"PanelView.Show(context='{context}', data={data})");
+    }
+}
