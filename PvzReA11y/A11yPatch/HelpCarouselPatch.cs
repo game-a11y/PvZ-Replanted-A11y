86c1e4c53e0e178ddd91e1c5a304187e06b5fd4f
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-01 00:27:42 +0800

    sr: 帮助菜单单独处理手柄

--- PvzReA11y/A11yPatch/HelpCarouselPatch.cs
+++ PvzReA11y/A11yPatch/HelpCarouselPatch.cs
@@ -46,8 +46,9 @@ public class HelpCarouselPatch
         a11yCtx += $";  currentScreen={currentScreen}";
 
         // TODO: 拆分帮助文本显示
-        // NOTE: 使用手柄时，pageNum=3 为“控制“页
-        // bool IsUsingController = InputManager.IsAnyPlayerUsingController();
+        //bool IsUsingController = InputService.IsAnyPlayerUsingController();
+        // NOTE: 只有控制器会多出“控制”页，故直接检测 pageNumStr 是否包含“6”字符
+        bool IsUsingController = pageNumStr.Contains('6');
         string a11yText = "";
         switch (currentScreen)
         {
@@ -58,13 +59,17 @@ public class HelpCarouselPatch
                 a11yText = $"实用帮助 ({pageNumStr})";
                 break;
             case 2:
-                a11yText = $"合作模式 ({pageNumStr})";
+                // NOTE: 使用手柄时，pageNum=3 为“控制“页
+                a11yText = IsUsingController ? $"控制" : $"合作模式";
+                a11yText = $"{a11yText} ({pageNumStr})";
                 break;
             case 3:
-                a11yText = $"对战模式 ({pageNumStr})";
+                a11yText = IsUsingController ? $"合作模式" : $"对战模式";
+                a11yText = $"{a11yText} ({pageNumStr})";
                 break;
             case 4:
-                a11yText = $"冒险安息 ({pageNumStr})";
+                a11yText = IsUsingController ? $"对战模式" : $"冒险安息";
+                a11yText = $"{a11yText} ({pageNumStr})";
                 break;
             case 5:
                 a11yText = $"冒险安息 ({pageNumStr})";
@@ -74,7 +79,7 @@ public class HelpCarouselPatch
                 break;
         }
 
-        A11y.SR.SpeakQueue(a11yText, a11yCtx);
+        A11y.SR.SpeakInterrupt(a11yText, a11yCtx);
     }
 
     [HarmonyPatch("SetPage")]
