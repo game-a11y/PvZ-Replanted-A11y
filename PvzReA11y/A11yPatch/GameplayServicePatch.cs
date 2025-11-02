5dfcb2c8bdc2d198552ce6975ba0053015758054
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-02 20:20:56 +0800

    hook: GameplayService

---
+++ PvzReA11y/A11yPatch/GameplayServicePatch.cs
@@ -0,0 +1,25 @@
+using HarmonyLib;
+using Il2CppReloaded.Services;
+using Il2CppReloaded.Data;
+
+namespace PvzReA11y.A11yPatch;
+
+[HarmonyPatch(typeof(GameplayService))]
+public static class GameplayServicePatch
+{
+    // SetCurrentLevelData 后置挂钩
+    [HarmonyPatch(nameof(GameplayService.SetCurrentLevelData))]
+    [HarmonyPostfix]
+    public static void SetCurrentLevelData_Postfix(GameplayService __instance, LevelEntryData levelData)
+    {
+        Core.gLogger.Msg($"GameplayService.SetCurrentLevelData(levelData.name={levelData?.m_levelName})");
+    }
+
+    // ClearCurrentLevelData 后置挂钩
+    [HarmonyPatch(nameof(GameplayService.ClearCurrentLevelData))]
+    [HarmonyPostfix]
+    public static void ClearCurrentLevelData_Postfix()
+    {
+        Core.gLogger.Msg("GameplayService.ClearCurrentLevelData()");
+    }
+}
