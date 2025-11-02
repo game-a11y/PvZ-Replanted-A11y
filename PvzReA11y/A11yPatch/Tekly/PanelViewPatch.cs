0e0f46ce3a7daad51ca4d43d6b4e9c24c739a407
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-02 15:24:06 +0800

    hook: PanelView.Show

--- PvzReA11y/A11yPatch/Tekly/PanelViewPatch.cs
+++ PvzReA11y/A11yPatch/Tekly/PanelViewPatch.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Text;
+using HarmonyLib;
 using Il2CppTekly.PanelViews;
 
 namespace PvzReA11y.A11yPatch.Tekly;
@@ -6,10 +7,16 @@ namespace PvzReA11y.A11yPatch.Tekly;
 [HarmonyPatch(typeof(PanelView))]
 internal class PanelViewPatch
 {
-    [HarmonyPatch(nameof(PanelView.Show))]
+    [HarmonyPatch(nameof(PanelView.Show), new Type[] { typeof(string), typeof(PanelData) })]
     [HarmonyPostfix]
-    public static void Show_Postfix(string context, PanelData data = null)
+    public static void Show_Postfix(PanelView __instance, string context, PanelData data)
     {
-        Core.gLogger.Msg($"PanelView.Show(context='{context}', data={data})");
+        if (__instance == null) return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"PanelView.Show(context='{context}', data={data})");
+        sb.Append($": Id={__instance.Id}");
+
+        Core.gLogger.Msg(sb.ToString());
     }
 }
