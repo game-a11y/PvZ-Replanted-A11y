7ccebe5dc77d4f92d293cbda30ab86717b97458b
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-02 15:28:30 +0800

    hook: MessageWidgetController.Show)

--- PvzReA11y/A11yPatch/MessageWidgetControllerPatch.cs
+++ PvzReA11y/A11yPatch/MessageWidgetControllerPatch.cs
@@ -1,6 +1,7 @@
 using HarmonyLib;
 using Il2CppSource.Controllers;
 using PvzReA11y.A11y;
+using System.Text;
 
 namespace PvzReA11y.A11yPatch;
 
@@ -9,8 +10,17 @@ public class MessageWidgetControllerPatch
 {
     [HarmonyPostfix]
     [HarmonyPatch(nameof(MessageWidgetController.Show))]
-    public static void Show_Postfix()
+    public static void Show_Postfix(MessageWidgetController __instance)
     {
-        Core.gLogger.Msg($"MessageWidgetController.Show called");
+        if (__instance == null) return;
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append($"MessageWidgetController.Show()");
+        sb.Append($": .k_panelId={MessageWidgetController.k_panelId}");
+        sb.Append($", .s_labelKey={MessageWidgetController.s_labelKey}");
+        sb.Append($", .m_isShowing={__instance.m_isShowing}");
+        sb.Append($", .m_prevString={__instance.m_prevString}");
+
+        Core.gLogger.Msg(sb.ToString());
     }
 }
